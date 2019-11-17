d21cb9d56ed78ec6aa96fa6d79e04ffea0fa106e
Nikita Andreev <nikita.andreev@uchi.ru>
2019-11-18 02:54:56 +0300

    change loader icon, change movie img position

--- src/components/movie/movie.js
+++ src/components/movie/movie.js
@@ -3,13 +3,14 @@ import './movie.scss';
 import placeholderImg from './placeholder.png';
 
 export default ({ title, director, imageUrl }) => {
+  const url = imageUrl === 'default' ? placeholderImg : imageUrl;
+
   return (
     <div className="movie">
       <div className="movie__content">
-        <img
+        <div
           className="movie__image"
-          src={imageUrl === 'default' ? placeholderImg : imageUrl}
-          alt="movie poster"
+          style={{ background: `url(${url}) center / cover no-repeat` }}
         />
         <div className="movie__text">
           <h4 className="movie__title">{title}</h4>
