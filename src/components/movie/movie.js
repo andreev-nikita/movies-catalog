fe5f9be2f6dba07fbf9e2aad1d916533ec46adfc
Nikita Andreev <nikita.andreev@uchi.ru>
2019-11-18 16:33:03 +0300

    add transition for the movie element

--- src/components/movie/movie.js
+++ src/components/movie/movie.js
@@ -2,21 +2,36 @@ import React from 'react';
 import './movie.scss';
 import placeholderImg from './placeholder.png';
 
-export default ({ title, director, imageUrl }) => {
-  const url = imageUrl === 'default' ? placeholderImg : imageUrl;
+export default class Movie extends React.Component {
+  state = {
+    opacity: 0,
+  };
 
-  return (
-    <div className="movie">
-      <div className="movie__content">
-        <div
-          className="movie__image"
-          style={{ background: `url(${url}) center / cover no-repeat` }}
-        />
-        <div className="movie__text">
-          <h4 className="movie__title">{title}</h4>
-          <p className="movie__director">{director}</p>
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ opacity: 1 });
+    }, 10);
+  }
+
+  render() {
+    const { title, director, imageUrl } = this.props;
+    const { opacity } = this.state;
+
+    const url = imageUrl === 'default' ? placeholderImg : imageUrl;
+
+    return (
+      <div className="movie" style={{ opacity }}>
+        <div className="movie__content">
+          <div
+            className="movie__image"
+            style={{ background: `url(${url}) center / cover no-repeat` }}
+          />
+          <div className="movie__text">
+            <h4 className="movie__title">{title}</h4>
+            <p className="movie__director">{director}</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
